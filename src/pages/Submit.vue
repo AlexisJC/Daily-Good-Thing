f0086f6b30811d44329efe54ce5d751c1fcf46a8
alexisjc <alexisjc.dev@gmail.com>
2019-09-01 15:28:41 +0200

    style: change submit btn

--- src/pages/Submit.vue
+++ src/pages/Submit.vue
@@ -23,7 +23,7 @@
           <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="file" type="file">
         </div>
         <div class="flex items-center justify-between">
-          <button class="bg-yellow-100 hover:bg-yellow-300 border border-black-500 text-black font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+          <button class="hover:bg-teal-100 border border-black-500 text-black font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
             Submit
           </button>
         </div>
