fde72a88962f679fbfc2ab2563e74c893ee2f7cc
Alexis JC <alexisjc.dev@gmail.com>
2020-05-27 21:47:51 +0200

    fix: input fields names are missing

--- src/pages/Submit.vue
+++ src/pages/Submit.vue
@@ -8,20 +8,20 @@
           <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
             Your name
           </label>
-          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Your name">
+          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" name="username" type="text" placeholder="Your name">
           <p class="text-xs italic mt-1">I will credit you !</p>
         </div>
         <div class="mb-4">
           <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
             Your email
           </label>
-          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Your email">
+          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" name="email" type="email" placeholder="Your email">
         </div>
         <div class="mb-6">
           <label class="block text-gray-700 text-sm font-bold mb-2" for="file">
             File
           </label>
-          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="file" type="file">
+          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="file" name="file" type="file">
         </div>
         <div class="flex items-center justify-between">
           <button class="hover:bg-teal-100 border border-black-500 text-black font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
