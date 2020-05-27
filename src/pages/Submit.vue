a599d0aec42def01add343c026e1cedc7ea5a154
Alexis JC <alexisjc.dev@gmail.com>
2020-05-27 22:00:37 +0200

    fix: add honeypot value again

--- src/pages/Submit.vue
+++ src/pages/Submit.vue
@@ -3,7 +3,7 @@
     <h1 class="text-xl md:text-3xl font-bold text-center my-5 text-gray-800">Your good thing can be published !</h1>
     <div class="w-full max-w-xs mx-auto my-10">
       <form method="POST" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" name="goodthing" data-netlify="true" data-netlify-honeypot="bot-field">
-        <input type="hidden" id="custId" name="form-name" value="">
+        <input type="hidden" id="custId" name="form-name" value="goodthing">
         <div class="mb-4">
           <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
             Your name
